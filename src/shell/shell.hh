84ac1eb5a5740a0cc8aaec2b2954c94e5cd3c457
Christian Kindahl <devnull@localhost>
2011-03-01 13:37:39 +0100

    Renamed default MSI package.

--- src/shell/shell.hh
+++ src/shell/shell.hh
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Tue Mar 01 13:34:07 2011
+/* at Tue Mar 01 13:36:35 2011
  */
 /* Compiler settings for .\shell.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
